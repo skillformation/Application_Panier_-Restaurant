a91b3a2ab95d233d006e34ea8dbd994a785ace4f
Blackamdev <outback.mimes_0d@icloud.com>
2025-07-31 10:33:26 -0400

    Créer 5 catégories,Pour chaque catégorie créer entre 5 et 20 produits

--- RestaurantApp/database/seeders/ProductsTableSeeder.php
+++ RestaurantApp/database/seeders/ProductsTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -11,7 +13,17 @@ class ProductsTableSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        
+    {   
+        // Créer 5 catégories
+        Category::factory(5)->create();
+        Product::factory(10)->create();
+        //Pour chaque catégorie créer entre 5 et 20 produits
+        Category::all()->each(function ($category) {
+            $productsCount = rand(5, 20);
+            Product::factory($productsCount)->create([
+                'category_id' => $category->id,
+            ]);
+        });
+
     }
 }
