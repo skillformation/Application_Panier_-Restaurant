a1ca833e3dd7c1c4790ce5d3ffbc1f806a6cc749
Blackamdev <outback.mimes_0d@icloud.com>
2025-07-31 10:26:24 -0400

    Creation et configuration du Factory de Category et Products

---
+++ RestaurantApp/database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        
+            'name' => $this->faker->unique()->word,
+            'icon' => $this->faker->randomElement(['fa-box', 'fa-tshirt', 'fa-laptop', 'fa-book', 'fa-utensils', null]),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}
---
+++ RestaurantApp/database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'price' => $this->faker->randomFloat(2, 1, 1000),
+            'image' => $this->faker->optional()->imageUrl(640, 480, 'products'),
+            'description' => $this->faker->paragraphs(3, true),
+            'category_id' => \App\Models\Category::factory(),
+            'is_active' => $this->faker->boolean(90), // 90% de chance d'être actif
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}
